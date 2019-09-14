be457e7b5e340af6cdb03a432a861aebddceed1d
Ben Arundel <ben.arundel@cdl.co.uk>
2019-09-14 23:34:48 +0100

    correcting settings block name:

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@ resource "aws_ecs_cluster" "cluster" {
   name = var.cluster_name
   tags = var.tags
 
-  settings {
+  setting {
     name = "containerInsights"
     value = var.enable_container_insights
-
   }
 
 }
